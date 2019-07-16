6e9cb1303a46324aa35a1f043346ef682fe0cd66
derek_ramm <derek_ramm@icloud.com>
2019-07-16 17:05:58 +0800

    123

---
+++ unit59_网络编程_udp/ex01_socket_server_udp.py
@@ -0,0 +1,18 @@
+#!/usr/bin/evn python
+# -*- coding:utf-8 -*-
+"""ex01_socket_server_udp.py"""
+
+# 服务端
+import socket
+ip_port = ('127.0.0.1', 9999)
+sk = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, 0)
+sk.bind(ip_port)
+
+while True:
+    data = sk.recv(1024).strip().decode()
+    print(data)
+    if data == "exit":
+        print("客户端主动断开连接！")
+        break
+
+sk.close()
